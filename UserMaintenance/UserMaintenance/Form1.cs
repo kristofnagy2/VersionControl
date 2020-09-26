d104088a2c0a96766ec1165181de4ece09f2d42a
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-09-26 15:13:50 +0200

    step7_2

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
 
             listBox1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+
+            {
+
+                FullName = textBox1.Text
+
+
+            };
+
+            users.Add(u);
+
+
         }
     }
 }
