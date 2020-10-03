01ed2ce2eeb921802c9746a6ef54903571979182
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-10-03 14:33:26 +0200

    step6

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -17,6 +17,9 @@ namespace gyak4
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +31,33 @@ namespace gyak4
         {
 
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel ()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                //CreateTable(); 
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+                
+            }
+        
         }
     
     }
