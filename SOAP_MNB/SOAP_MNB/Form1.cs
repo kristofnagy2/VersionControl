550e8bb39799b8dcd965936b4175c68b941353d8
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-10-18 12:37:43 +0200

    step7_2

--- SOAP_MNB/SOAP_MNB/Form1.cs
+++ SOAP_MNB/SOAP_MNB/Form1.cs
@@ -19,16 +19,27 @@ namespace SOAP_MNB
 
         BindingList<RateData> Rates = new BindingList<RateData>();
         XmlDocument Xml = new XmlDocument();
-        
+
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+
+
+        }
+
+        private void RefreshData()
+
+        {
+
+            Rates.Clear();
+
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
@@ -66,10 +77,24 @@ namespace SOAP_MNB
                 chartArea.AxisX.MajorGrid.Enabled = false;
                 chartArea.AxisY.MajorGrid.Enabled = false;
                 chartArea.AxisY.IsStartedFromZero = false;
-            }
 
+
+            }
         }
 
-     
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
