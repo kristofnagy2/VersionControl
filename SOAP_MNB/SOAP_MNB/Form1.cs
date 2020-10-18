5c6c46c53d62a58c93ced9647c717cfe6bdf1433
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-10-18 11:58:19 +0200

    step3

--- SOAP_MNB/SOAP_MNB/Form1.cs
+++ SOAP_MNB/SOAP_MNB/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SOAP_MNB.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,22 @@ namespace SOAP_MNB
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+                
         }
+
+   
     }
 }
