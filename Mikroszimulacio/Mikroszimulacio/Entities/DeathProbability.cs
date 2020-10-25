b193c4b71eed7691ee17d6612396d3cfc7747280
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-10-25 19:38:26 +0100

    step4

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/DeathProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    class DeathProbability
+    {
+
+        public int Gender { get; set; }
+        public int Age { get; set; }
+
+        public double DeathPossibility { get; set; }
+
+    }
+}
