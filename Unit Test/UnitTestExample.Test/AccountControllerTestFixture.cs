54fed35b7835e85f0e4b687b226d08681aa0e5da
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-11-22 17:40:15 +0100

    step7

--- Unit Test/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Unit Test/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -6,23 +6,36 @@ namespace UnitTestExample.Test
 
     public class AccountControllerTestFixture
     {
-       
-       
-      
-        [
 
-            Test,
-            TestCase("abcd1234", false),
-            TestCase("irf@uni-corvinus", false),
-            TestCase("irf.uni-corvinus.hu", false),
-            TestCase("irf@uni-corvinus.hu", true)
 
-        ]
+
+       
+
         public void TestValidateEmail(string email, bool expectedResult)
         {
             var accountController = new AccountController();
             var actualResult = accountController.ValidateEmail(email);
             Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+        [
+
+               Test,
+               TestCase("abcdefgh", false),
+               TestCase("ABCD1234", false),
+               TestCase("abcd1234", false),
+               TestCase("abcd123", false),
+               TestCase("Abcd1234", true)
+
+
+          ]
+
+
+  
+        public void TestValidatePassword(string password, bool expectedResult)
+        { 
+        
+        
         }
     }
 }
