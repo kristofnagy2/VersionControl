45962488cfda7a79270ec41d7dbe4b8a749db936
Kristóf Nagy <kristof.nagy2@stud.uni-corvinus.hu>
2020-11-22 17:19:44 +0100

    commit

--- Unit Test/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ Unit Test/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,19 +1,24 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
+
     public class AccountControllerTestFixture
     {
 
-        [Test]
+       
+      
+        [
+            Test,
+
+            
+            
+            
+        ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            // ...
+            
         }
     }
 }
